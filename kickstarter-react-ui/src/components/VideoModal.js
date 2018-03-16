74657771936bc101811a99a3f6fa715f47c776cd
Jaskaran Singh <s.jaskaran4@gmail.com>
2018-03-16 23:32:19 +0000

    added a new route to test modal for video

---
+++ kickstarter-react-ui/src/components/VideoModal.js
@@ -0,0 +1,95 @@
+// import React, { Component } from 'react';
+// import {Button, Modal, Tooltip, OverlayTrigger, Popover, Row, Col, PageHeader} from 'react-bootstrap';
+// import Demo from './Demo'
+
+// class Example extends React.Component {
+//   constructor(props, context) {
+//     super(props, context);
+
+//     this.handleShow = this.handleShow.bind(this);
+//     this.handleClose = this.handleClose.bind(this);
+
+//     this.state = {
+//       show: false
+//     };
+//   }
+
+//   handleClose() {
+//     this.setState({ show: false });
+//   }
+
+//   handleShow() {
+//     this.setState({ show: true });
+//   }
+
+//   render() {
+//     const popover = (
+//       <Popover id="modal-popover" title="popover">
+//         very popover. such engagement
+//       </Popover>
+//     );
+//     const tooltip = <Tooltip id="modal-tooltip">wow.</Tooltip>;
+
+//     return (
+//       <div>
+//         <p>Placeholder 1</p>
+
+//         <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
+//           Launch demo modal
+//         </Button>
+
+//         <Modal show={this.state.show} onHide={this.handleClose}>
+//           <Modal.Header closeButton>
+//             <Modal.Title>Modal heading</Modal.Title>
+//           </Modal.Header>
+//           <Modal.Body>
+//             <h4>Text in a modal</h4>
+//             <p>
+//               Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+//             </p>
+
+
+//             <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + demo.videos}  frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+//             <h4>Popover in a modal</h4>
+//             <p>
+//               there is a{' '}
+//               <OverlayTrigger overlay={popover}>
+//                 <a href="#popover">popover</a>
+//               </OverlayTrigger>{' '}
+//               here
+//             </p>
+
+//             <h4>Tooltips in a modal</h4>
+//             <p>
+//               there is a{' '}
+//               <OverlayTrigger overlay={tooltip}>
+//                 <a href="#tooltip">tooltip</a>
+//               </OverlayTrigger>{' '}
+//               here
+//             </p>
+
+//             <hr />
+
+//             <h4>Overflowing text to show scroll behavior</h4>
+//             <p>
+//               Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+//               dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
+//               ac consectetur ac, vestibulum at eros.
+//             </p>
+//             <p>
+//               Praesent commodo cursus magna, vel scelerisque nisl consectetur
+//               et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
+//               auctor.
+//             </p>
+//           </Modal.Body>
+//           <Modal.Footer>
+//             <Button onClick={this.handleClose}>Close</Button>
+//           </Modal.Footer>
+//         </Modal>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Example

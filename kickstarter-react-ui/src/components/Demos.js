ce8c2e33dc14479f80edcbdcf637b7ee7ae2f430
Jaskaran Singh <s.jaskaran4@gmail.com>
2018-03-16 23:31:48 +0000

    changed sorting UI from tabs to navbar

--- kickstarter-react-ui/src/components/Demos.js
+++ kickstarter-react-ui/src/components/Demos.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 import Demo from './Demo'
 import TopNav from './TopNav'
+import {DropdownButton, MenuItem, ButtonToolbar, Tab, Tabs} from 'react-bootstrap'
+
 // import Dialog from './Dialog'
 // import Modal from './Modal';
 
 // import $ from 'jquery'
 // import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button} from 'reactstrap'
-import {Grid, Button, Thumbnail, Row, Col, PageHeader, Modal, Table} from 'react-bootstrap'
+import {Grid, Button, Navbar, Nav, NavItem, NavDropdown, Thumbnail, Row, Col, PageHeader, Modal, Table} from 'react-bootstrap'
 import {Route, Switch, Link} from 'react-router-dom'
 
   // Client-side model
 import Resource from '../utilities/resource'
+
 const DemoStore = Resource('demos')
 
 
@@ -61,36 +64,71 @@ class Demos extends React.Component {
   //   .then((result) => console.log(result))//this.setState({demos: result.data, errors: null}))
   //   .catch((errors) => this.setState({errors: errors}))
   // }
-
   render() { 
-    return ( 
+    return (
+      <div>
+      <Navbar>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a href="/demos">Trending</a>
+    </Navbar.Brand>
+  </Navbar.Header>
+  <Nav>
+    {/*<NavItem eventKey={1} href="#">
+      Link
+    </NavItem>
+    <NavItem eventKey={2} href="#">
+      Link
+    </NavItem>*/}
+    <NavDropdown eventKey={3} title="Sort by Category" id="basic-nav-dropdown">
+      <MenuItem eventKey={3.1}>Web Apps</MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey={3.2}>iOS Apps</MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey={3.3}>UI/UX Apps</MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey={3.4}>Products</MenuItem>
+    </NavDropdown>
+
+    <NavDropdown eventKey={3} title="Sort by Location" id="basic-nav-dropdown">
+      <MenuItem eventKey={3.1}>LHL Toronto</MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey={3.2}>LHL Montreal</MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey={3.3}>Brainstation</MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey={3.4}>Hacker</MenuItem>
+    </NavDropdown>
+  </Nav>
+</Navbar>
       <div>
         {/*<TopNav user={this.state.user}/>*/}
+        <Grid>
+          <Row>
+            {this.state.demos.map((demo, id) => {
+              return (
 
-        {this.state.demos.map((demo, id) => {
-          return (
-            <Grid>
-              <Row>
-                <Col xs={6} md={4}>
-                  <Thumbnail src="https://img.youtube.com/vi/bCb6JEjbwhQ/0.jpg">
-                    <h3>{demo.name}</h3>
-                    <p>{demo.description}</p>
-                    <p>{demo.category.name}</p>
-                    <p>{demo.event.name}</p>
-                    <p>{demo.live}</p>
-                    <p>{demo.fundingreq}</p>
-                    <p>{demo.votes}</p>
-                    <p>
-                      <Button bsStyle="primary" href="/login">Contribute</Button>&nbsp;
-                      <Button bsStyle="default">View More</Button>
-                    </p>
-                  </Thumbnail>
-                </Col>
-              </Row>
-            </Grid>
-            )
-        })
-      }
+                    <Col xs={6} md={4}>
+                      <Thumbnail src={"https://img.youtube.com/vi/" + demo.videos + "/0.jpg"}>
+                        <h3>{demo.name}</h3>
+                        <p>{demo.description}</p>
+                        <p>{demo.category.name}</p>
+                        <p>{demo.event.name}</p>
+                        <p>{demo.live}</p>
+                        <p>{demo.fundingreq}</p>
+                        <p>{demo.votes}</p>
+                        <p>{demo.videos}</p>
+                        <p>
+                          <Button bsStyle="primary" href="/login">Contribute</Button>&nbsp;
+                          <Button bsStyle="default" href={"https://www.youtube.com/watch?v="+demo.videos}>View Video</Button>
+                        </p>
+                      </Thumbnail>
+                    </Col>
+              )
+            })}
+          </Row>
+        </Grid>
+      </div>
       </div>
     )
   }

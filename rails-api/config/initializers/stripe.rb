7fb0c9799e098a2300e0f856faaa0b2ef5176589
Jaskaran Singh <s.jaskaran4@gmail.com>
2018-03-21 00:48:44 +0000

    spacing

--- rails-api/config/initializers/stripe.rb
+++ rails-api/config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => ENV['pk_test_s4jPh9mguQ56Sy5fDmsUxZ0e'],
+  :secret_key      => ENV['sk_test_xQOntrvG66aj3QQFDZ8ho0xf']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
